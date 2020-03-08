24d1ddadd4d36c2a21bcffafc31d01bc233af4f0
zhengquan <zheng_quan45@163.com>
2020-03-08 13:58:13 +0800

    feat: session store in redis

--- is-server-auth/src/main/java/org/zhq/security/server/auth/OAuth2AuthServerConfig.java
+++ is-server-auth/src/main/java/org/zhq/security/server/auth/OAuth2AuthServerConfig.java
@@ -13,10 +13,13 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import javax.sql.DataSource;
 
+
 @Configuration
+@EnableRedisHttpSession
 @EnableAuthorizationServer
 public class OAuth2AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
